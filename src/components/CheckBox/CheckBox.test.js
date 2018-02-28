74c75357af01671aae03fdc68ac4124e3d6cb03e
ethan.nguyen <ethan.nguyens@gmail.com>
2018-02-28 13:53:19 +0000

    tests for CheckBox Comp

---
+++ src/components/CheckBox/CheckBox.test.js
@@ -0,0 +1,37 @@
+import expect from 'expect';
+import React from 'react';
+import {mount} from 'enzyme';
+import CheckBox from './CheckBox';
+import initialStates from '../../reducers/initialState';
+import configureStore from 'redux-mock-store';
+import {Provider} from 'react-redux';
+
+function setup() {
+  const props = initialStates.instaFlickr;
+  const selectedItems  = [0, 1];
+  let mockState = Object.assign({}, initialStates, {checkBox: {
+    items: ["One", "Two", "Three"],
+    selectedItems: selectedItems
+  }});
+  const createStore = (state = mockState) => {
+    const middleWares = [];
+    const mockStore = configureStore(middleWares);
+    return mockStore(state);
+  };
+  const store = createStore();
+
+  const wrapper = mount(<Provider store={store}><CheckBox/></Provider>);
+  return {
+    props,
+    wrapper
+  }
+}
+
+describe('CheckBox Component', () => {
+  it('should render itself', () => {
+    // Arrange & Act
+    const {wrapper} = setup();
+    // Assert
+    expect(wrapper.find('CheckBox').hasClass('CheckBox')).toBe(true);
+  });
+});
