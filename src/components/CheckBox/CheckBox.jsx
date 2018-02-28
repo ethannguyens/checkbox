4572750ef1cdfb75e56193446cea7bc77aee94c3
ethan.nguyen <ethan.nguyens@gmail.com>
2018-02-28 13:37:37 +0000

    add check box comp and change index route

---
+++ src/components/CheckBox/CheckBox.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as checkBoxActions from '../../actions/checkBoxAction';
+
+class CheckBox extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+  }
+
+  render() {
+    return (
+      <div className="CheckBox">
+        {this.props.checkBox.items.map((item, i) => {
+          return (
+            <div key={i} className="CheckBox-li" >
+              <span className="CheckBox-radio" />
+              <span className="CheckBox-label">{item}</span>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
+}
+
+CheckBox.propTypes = {
+  checkBox: PropTypes.object.isRequired
+};
+
+function mapStateToProps(state, ownProps) {
+  return {
+    checkBox: state.checkBox
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(checkBoxActions, dispatch)
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CheckBox);
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './components/App.jsx';
+import CheckBox from './components/CheckBox/CheckBox'
 
 export default (
   <Route path="/" component={App}>
-    <IndexRoute component={App} />
+    <IndexRoute component={CheckBox} />
   </Route>
 );
